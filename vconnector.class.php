af55e02ac83380b5c4ec610c1a82722f66c08c0e
VIANCH Repository <vachavarro@gmail.com>
2012-08-02 17:20:39 -0500

    Funciones completas
    Funciones completas del vconnector

--- vconnector.class.php
+++ vconnector.class.php
@@ -52,32 +52,163 @@ class vconnector{
 	 * INSERTA EN UNA TABLA DETERMINADA
 	 * se pasan por parámetros las columnas a insertar (string: columnas separadas por coma) y los valores
 	 * que van en ellas (string: valores separados por coma)
+	 * ejemplo 
+	 * $table_name = 'table_user';
+	 * $cols = 'id,name_user';
+	 * $vals = "1,'juan'";
+	 *
+	 * SQL Generado: INSERT INTO table_user (id,name_user) VALUES (1, 'juan')
 	 * 
 	 * @param string $table_name
 	 * @param string $cols
 	 * @param string $vals
+	 *
+	 * @return true;
 	 */
 	public function insert($table_name,$cols,$vals){
 		$SQL_INSERT = "INSERT INTO $table_name ($cols) VALUES ($vals)";
-		$answer = mysql_query($SQL_INSERT1,$conexion);
+		$answer = mysql_query($SQL_INSERT1,$this->conexion);
 		if(!$answer){
 			 die('INSERT no válida: ' . mysql_error());
 		}
+		else{
+			return true;
+		}
+		
 	}
 	
-    public function select($tables){
+	/**
+	 * Como realizar un select es diverso y dependiendo de como se quiera consultar
+	 * esta función recibe por parametro el SQL que contiene el SELECT y opcional
+	 * un debug para saber que esta pasando con el SQL
+	 * la función retorna un array con toda la información de la consulta SQL, retorna
+	 * array vacio si se genero algún error
+	 * 
+	 * @param string $SQL
+	 * @param bool $debug
+	 *
+	 * @return array
+	 * @see debuger() para ver como se retorna el debug del select
+	 */
+    public function select($SQL,$debug = false){
+		$array_return = array();
+		$answer = mysql_query($SQL,$this->conexion);
+		$num_rows = mysql_num_rows($answer);
+		if($answer && ($num_rows > 0)){
+			while ($row = mysql_fetch_assoc($answer)) {													
+				$array_return[] = $row;
+			}
+		}
+		
+		/*debug para revisar si el sql que se pasa es el correcto y las respuestas del array*/
+		if($debug){
+			$this->debuger($SQL,$array_return);
+		}
+		
+		return $array_return;
+	}
 
-	
-	}
 
-	public function update(){
+	/**
+	 * ACTUALIZA UNA TABLA DETERMINADA 
+	 * Recibe por parametro la tabla a acutalizar, que columnas se van actualizar,
+	 * cuales son los nuevos valores y las condiciones si tiene de que se va actualizar
+	 * EJ: 
+	 * $table = "user";
+	 * $cols = "name,age,..."
+	 * $vals = "carlos,30,..";
+	 * $clauses = "ID = 1,..."
+	 *
+	 * SQL Generado: UPDATE user SET name = 'carlos', age = '30' WHERE 1=1 AND ID = 1;
+	 *
+	 * @param string $table
+	 * @param string $cols
+	 * @param string $vals
+	 * @param string $clauses
+	 *
+	 * @return true;
+	 */
+	public function update($table, $cols, $vals, $clauses = ''){
+		$AND = '1=1 ';
+		$columns = explode(',', $cols);
+		$values = explode(',', $vals);
+		$clauses = explode(',', $clauses);
+
+		if((count($columns)) != (count($values))){
+			return false;
+		}
+		else{
+			$sets = '';
+			$i=0;
+			foreach ($columns as $column) {
+				$sets .="$column = '".$values[$i]."', ";
+				++$i;
+			}
+			foreach ($clauses as $clause) {
+				$AND .="AND $clause ";
+			}
+
+			$sets = substr($sets , 0, -2); //quita la ',' y el espacio final
+			$AND = substr($AND , 0, -1); //quita el espacio final
+			
+			$SQL_UPDATE = "UPDATE $table SET $sets WHERE $AND";
+			$answer = mysql_query($SQL_UPDATE,$this->conexion);
+			if(!$answer){
+				 die('actualización no válida: ' . mysql_error());
+			}
+			else{
+				return true;
+			}
+		}
+		
 
 	}
 	
-	public function delete(){
-	
+	/**
+	 * BORRA UN REGISTRO DE LA BASE DE DATOS
+	 * Recibe como parametros la tabla de donde va ser borrado el registro y 
+     * las condiciones de borrado	
+	 * 
+	 * $table = "user"
+	 * $clauses = "ID = 1"
+	 *
+	 * SQL GENERADO: DELETE FROM user WHERE 1=1 AND ID = 1
+	 *
+	 * @param string $table
+	 * @param string $clauses
+	 *
+	 * @return true;
+	 */
+	public function delete($table, $clauses = ''){
+		$AND = '1=1 ';
+		$clauses = explode(',', $clauses);
+		foreach ($clauses as $clause) {
+				$AND .="AND $clause ";
+		}
+		$SQL_DELETE = "DELETE FROM $table WHERE $AND";
+		$answer = mysql_query($SQL_DELETE,$this->conexion);
+		if(!$answer){
+			 die('delete no válido: ' . mysql_error());
+		}
+		else{
+			return true;
+		}
 	}
 	
+	/**
+	 *DEBUGER DE SQLS 
+	 *
+	 */
+	$public function debuger($SQL,$ANSWER){
+		echo "SQL-> ".$SQL;
+		echo "RETORNO: "; print_r($ANSWER);
+	}
+	
+	
+	
+	/**
+	 * destruye la conexión
+	 */
 	public function __destruct(){
 		mysql_close($this->conexion);
 		
